e420ee6e7f763d5b6606f5d62c6d27f0d3b824c1
Ahmet Arslan <arslanahmet1981@gmail.com>
2021-07-11 13:29:23 +0300

    testler eklendi

---
+++ UnitTests/TicketControllerTests.cs
@@ -0,0 +1,13 @@
+using Xunit;
+
+namespace UnitTests
+{
+    public class TicketControllerTests
+    {
+        [Fact]
+        public void GetSingleTicket_ValidId_BringTicket()
+        {
+            Assert.Equal("mail", "mail");
+        }
+    }
+}
