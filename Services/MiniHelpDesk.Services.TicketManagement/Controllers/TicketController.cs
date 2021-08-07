d61b0e8d72e929b0ca104a3a60066476ff30132f
Ahmet Arslan <arslanahmet1981@gmail.com>
2021-08-08 01:37:30 +0300

    get container name in response to check k8s

--- Services/MiniHelpDesk.Services.TicketManagement/Controllers/TicketController.cs
+++ Services/MiniHelpDesk.Services.TicketManagement/Controllers/TicketController.cs
@@ -94,7 +94,7 @@ namespace MiniHelpDesk.Services.TicketManagement.Controllers
         [HttpGet("{id}", Name = "GetTicket")]
         public async Task<IActionResult> GetTicketAsync(int Id, [FromQuery] string fields)
         {
-            return Ok();
+            return Ok(System.Environment.MachineName);
 
             if (!_typeHelperService.TypeHasProperties<TicketDto>(fields))
             {
