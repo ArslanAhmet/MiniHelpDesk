c86fd6874e741493957cfcc77c1d6dd1b1722ab2
Ahmet Arslan <arslanahmet1981@gmail.com>
2021-11-01 02:43:39 +0300

    Dapper will be used

--- Services/MiniHelpDesk.Services.TicketManagement/Core/Entities/Ticket.cs
+++ Services/MiniHelpDesk.Services.TicketManagement/Core/Entities/Ticket.cs
@@ -1,9 +1,23 @@
-﻿namespace MiniHelpDesk.Services.TicketManagement.Core.Entities
+﻿using System;
+
+namespace MiniHelpDesk.Services.TicketManagement.Core.Entities
 {
     public class Ticket : BaseEntity
     {
+        public int AssigneeID { get; set; }
+        public int BrandID { get; set; }
+        public string Channel { get; set; }
         public string Name { get; set; }
         public string Description { get; set; }
+        public DateTime DueDate { get; set; }
         public bool IsDeleted { get; set; }
+        public int OrganizationID { get; set; }
+        public int Priority { get; set; }
+        public int RequesterID { get; set; }
+        public int Satisfaction { get; set; }
+        public DateTime? SolveDate { get; set; }
+        public int Subject { get; set; }
+        public int SubmitterID { get; set; }
+        public int TicketTypeID { get; set; }
     }
 }
